3dacdb05b42b332b3800c84c33f36c419d9cbe72
Ela <krzeczekelzbieta@gmail.com>
2018-12-13 21:28:06 +0100

    admin_app update

--- emka_trans/admin_app/urls.py
+++ emka_trans/admin_app/urls.py
@@ -5,5 +5,8 @@ app_name = 'admin_app'
 
 urlpatterns = [
    url(r'^dashboard/$',views.IndexView.as_view(),name='index'),
-   url(r'^orders/$',views.AdminCheckoutListView.as_view(),name='list'),
+   url(r'^orders/$',views.AdminCheckoutListView.as_view(),name='order_list'),
+   url(r'^orders/(?P<pk>\d+)/$',views.AdminCheckoutDetailView.as_view(),name='order_detail'),
+   url(r'^products/$',views.AdminProductListView.as_view(),name='product_list'),
+
 ]
